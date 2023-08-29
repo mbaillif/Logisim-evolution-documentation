c018b5c6271b4b9bce471127c10716d8087c8d24
mbaillif <baim@baillifard.com>
2023-08-29 15:42:36 +0200

    upgrade to V3.9
    PLAROM, chronogramme

--- doc_fr.hs
+++ doc_fr.hs
@@ -13,11 +13,11 @@
    </maps>
 	
    <!-- views --> 
-   <view xml:lang="en" mergetype="javax.help.UniteAppendMerge">
+   <view xml:lang="fr" mergetype="javax.help.UniteAppendMerge">
       <name>TOC</name>
       <label>Table des matières</label>
       <type>javax.help.TOCView</type>
-      <data>en/contents.xml</data>
+      <data>fr/contents.xml</data>
    </view>
 
   <view>
